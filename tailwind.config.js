56a101d0e2615594a80e9f374d5a8c4559f66630
YOUR NAME <nguyentuanhung26072001@gmail.com>
2023-10-02 17:28:53 +0700

    new

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,21 @@ module.exports = {
         '.w-minus': {
           width: 'calc(100% - 263px)',
         },
+        '.font-roboto':{
+          fontFamily:'Roboto'
+        },
+        '.logo-color':{
+          backgroundColor:'rgba(243, 217, 217, 1)'
+        },
+        '.thead-background':{
+          backgroundColor:'rgba(181, 1, 62, 1)'
+        },
+        '.td1-color':{
+          backgroundColor:'rgba(205, 92, 92, 1)'
+        },
+        '.td2-color':{
+          backgroundColor:'rgba(229, 229, 229, 1)'
+        }
       });
     },
   ],
